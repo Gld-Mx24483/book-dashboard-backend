6daf8b175aa2b33cf9bb786f72b36f03cde7d22c
Gld_Mx24483 <israeltoki7@gmail.com>
2024-11-28 22:54:48 +0100

    Implementation of OAuth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,42 @@
+
 // import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+// import { GraphQLModule } from '@nestjs/graphql';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+// import { BookModule } from './book/book.module';
+// import { join } from 'path';
+// import { SwaggerModule } from '@nestjs/swagger';
 
 // @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'sqlite',
+//       database: './book-dashboard.sqlite',
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       synchronize: true,
+//     }),
+//     GraphQLModule.forRoot<ApolloDriverConfig>({
+//       driver: ApolloDriver,
+//       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+//       playground: true,
+//     }),
+//     BookModule,
+//   ],
 // })
 // export class AppModule {}
-
 
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { BookModule } from './book/book.module';
+import { AuthModule } from './auth/auth.module';
 import { join } from 'path';
-import { SwaggerModule } from '@nestjs/swagger';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: './book-dashboard.sqlite',
@@ -30,8 +47,10 @@ import { SwaggerModule } from '@nestjs/swagger';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
+      context: ({ req }) => ({ req }),
     }),
     BookModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,9 +1,54 @@
+// import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+// import { BookService } from './book.service';
+// import { Book } from './book.entity';
+// import { CreateBookInput } from './dto/create-book.input';
+// import { UpdateBookInput } from './dto/update-book.input';
+// import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
+// @Resolver(() => Book)
+// @ApiTags('books')
+// export class BookResolver {
+//   constructor(private bookService: BookService) {}
+
+//   @Query(() => [Book])
+//   @ApiOperation({ summary: 'Get all books' })
+//   async getBooks(): Promise<Book[]> {
+//     return this.bookService.findAll();
+//   }
+
+//   @Query(() => Book)
+//   @ApiOperation({ summary: 'Get a book by ID' })
+//   async getBook(@Args('id', { type: () => Int }) id: number): Promise<Book> {
+//     return this.bookService.findOne(id);
+//   }
+
+//   @Mutation(() => Book)
+//   @ApiOperation({ summary: 'Create a new book' })
+//   async createBook(@Args('createBookInput') createBookInput: CreateBookInput): Promise<Book> {
+//     return this.bookService.create(createBookInput);
+//   }
+
+//   @Mutation(() => Book)
+//   @ApiOperation({ summary: 'Update a book' })
+//   async updateBook(@Args('updateBookInput') updateBookInput: UpdateBookInput): Promise<Book> {
+//     return this.bookService.update(updateBookInput.id, updateBookInput);
+//   }
+
+//   @Mutation(() => Book)
+//   @ApiOperation({ summary: 'Delete a book' })
+//   async deleteBook(@Args('id', { type: () => Int }) id: number): Promise<Book> {
+//     return this.bookService.remove(id);
+//   }
+// }
+
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { BookService } from './book.service';
 import { Book } from './book.entity';
 import { CreateBookInput } from './dto/create-book.input';
 import { UpdateBookInput } from './dto/update-book.input';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Resolver(() => Book)
 @ApiTags('books')
@@ -12,30 +57,35 @@ export class BookResolver {
 
   @Query(() => [Book])
   @ApiOperation({ summary: 'Get all books' })
+  @UseGuards(JwtAuthGuard)
   async getBooks(): Promise<Book[]> {
     return this.bookService.findAll();
   }
 
   @Query(() => Book)
   @ApiOperation({ summary: 'Get a book by ID' })
+  @UseGuards(JwtAuthGuard)
   async getBook(@Args('id', { type: () => Int }) id: number): Promise<Book> {
     return this.bookService.findOne(id);
   }
 
   @Mutation(() => Book)
   @ApiOperation({ summary: 'Create a new book' })
+  @UseGuards(JwtAuthGuard)
   async createBook(@Args('createBookInput') createBookInput: CreateBookInput): Promise<Book> {
     return this.bookService.create(createBookInput);
   }
 
   @Mutation(() => Book)
   @ApiOperation({ summary: 'Update a book' })
+  @UseGuards(JwtAuthGuard)
   async updateBook(@Args('updateBookInput') updateBookInput: UpdateBookInput): Promise<Book> {
     return this.bookService.update(updateBookInput.id, updateBookInput);
   }
 
   @Mutation(() => Book)
   @ApiOperation({ summary: 'Delete a book' })
+  @UseGuards(JwtAuthGuard)
   async deleteBook(@Args('id', { type: () => Int }) id: number): Promise<Book> {
     return this.bookService.remove(id);
   }
